d1bdf2e7565e80c0f0860689ffbd4d9a66448685
Eli Huebner <elimhuebner@gmail.com>
2021-04-20 12:58:53 -0400

    adds space between amount and dollars in output

--- if_statement_practice/currency_conv.cpp
+++ if_statement_practice/currency_conv.cpp
@@ -12,11 +12,11 @@ int main() {
     std::cin >> amount >> unit;
 
     if (unit == 'y')
-        std::cout << amount << "Yen = " << amount * y_per_usd << "Dollars\n";
+        std::cout << amount << "Yen = " << amount * y_per_usd << " Dollars\n";
     else if (unit == 'k')
-        std::cout << amount << "Kroner = " << amount * k_per_usd << "Dollars\n";
+        std::cout << amount << "Kroner = " << amount * k_per_usd << " Dollars\n";
     else if (unit == 'p')
-        std::cout << amount << "GBP = " << amount * gbp_per_usd << "Dollars\n";
+        std::cout << amount << "GBP = " << amount * gbp_per_usd << " Dollars\n";
     else
         std::cout << "I don't know what " << unit << " means.\n";
 }
