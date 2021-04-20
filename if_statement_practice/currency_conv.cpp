d8f83f8a010eb69c76f6868f4df07e1096cb1244
Eli Huebner <elimhuebner@gmail.com>
2021-04-20 12:54:53 -0400

    adds if statement to check for unit input

--- if_statement_practice/currency_conv.cpp
+++ if_statement_practice/currency_conv.cpp
@@ -5,8 +5,18 @@ int main() {
     constexpr double k_per_usd = 0.12;
     constexpr double gbp_per_usd = 1.39;
 
+    int amount;
     char unit = ' ';
 
     std::cout << "Enter an amount of currency, followed by y(yen), k(kroner), or p(gbp), to be converted to dollars: ";
-    
+    std::cin >> amount >> unit;
+
+    if (unit == 'y')
+        std::cout << amount << "Yen = " << amount * y_per_usd << "Dollars\n";
+    else if (unit == 'k')
+        std::cout << amount << "Kroner = " << amount * k_per_usd << "Dollars\n";
+    else if (unit == 'p')
+        std::cout << amount << "GBP = " << amount * gbp_per_usd << "Dollars\n";
+    else
+        std::cout << "I don't know what " << unit << " means.\n";
 }
