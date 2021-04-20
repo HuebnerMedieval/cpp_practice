10b97cb08d0b55c85651ce7dd54ff693c5f30f87
Eli Huebner <elimhuebner@gmail.com>
2021-04-20 12:48:14 -0400

    adds prompt to input currency

--- if_statement_practice/currency_conv.cpp
+++ if_statement_practice/currency_conv.cpp
@@ -4,4 +4,9 @@ int main() {
     constexpr double y_per_usd = 0.0093;
     constexpr double k_per_usd = 0.12;
     constexpr double gbp_per_usd = 1.39;
+
+    char unit = ' ';
+
+    std::cout << "Enter an amount of currency, followed by y(yen), k(kroner), or p(gbp), to be converted to dollars: ";
+    
 }
