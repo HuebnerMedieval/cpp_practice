f73f044cc1b2f7af4463111bb02c1a9995cb95ac
Eli Huebner <elimhuebner@gmail.com>
2021-04-29 15:57:54 -0400

    adds comments to char_ints.cpp

--- while_loop_practice/char_ints.cpp
+++ while_loop_practice/char_ints.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 
+// main function
 int main() {
+    
+    // initializes and declares variable c as the letter 'a'
     char c = 'a';
+    // initializes and declared variable ic as the int value of the variable c
     int ic = c;
+
+    // repeats the block for each letter of the alphabet
     while (c <= 'z'){
+        // outputs c tabchar ic newline
         std::cout << c << "\t" << ic << "\n";
+        // iterates c to the next letter
         ++c;
+        // iterates ic to the next integet
         ++ic;
     }
 }
