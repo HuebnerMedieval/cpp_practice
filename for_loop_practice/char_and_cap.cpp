39fc64d52052401dc34cca9e17dddb7a384f1403
Eli Huebner <elimhuebner@gmail.com>
2021-04-29 16:26:11 -0400

    adds comments to char_an_cap.cpp

--- for_loop_practice/char_and_cap.cpp
+++ for_loop_practice/char_and_cap.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 
+// main function
 int main() {
+    // for loop to print characters and integers
     for (char c='a'; c<='z'; c++){
+        // initializes variable to convert c to int
         int ic = c;
+        // outputs the character and its integer value
+        // subtracting 32 from char points to ASCII for its capital
         std::cout << c << "\t" << ic
         << "\t" << char(c-32) << "\t" << ic-32 << "\n";
     }
