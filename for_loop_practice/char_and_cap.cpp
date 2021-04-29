61cdac55272bfe78d0bf8e3094579064f8a7aca4
Eli Huebner <elimhuebner@gmail.com>
2021-04-29 16:23:41 -0400

    adds for loop to print chars, ints, and capitals

--- for_loop_practice/char_and_cap.cpp
+++ for_loop_practice/char_and_cap.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
 int main() {
-    
+    for (char c='a'; c<='z'; c++){
+        int ic = c;
+        std::cout << c << "\t" << ic
+        << "\t" << char(c-32) << "\t" << ic-32 << "\n";
+    }
 }
